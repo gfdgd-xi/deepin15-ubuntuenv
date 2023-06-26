1ae38adb59574df4dc899e5808304993a539135e
gfdgd-xi <3025613752@qq.com>
2023-06-26 15:47:39 +0800

    优化

--- gui/runner.py
+++ gui/runner.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
+import os
 import sys
 import PyQt5.QtWidgets as QtWidgets
+
+def RunProgram():
+    window.hide()
+    if os.system("which gx-env-run"):
+        QtWidgets.QMessageBox.critical(window, "错误", "gx-env-run 不存在！")
+        return
+    if runProgramWithRoot.isChecked():
+        if os.getenv("HOME") + "/.config/gx-env/bwrap":
+            envDist = os.environ
+            envSet = "DISPLAY=$DISPLAY "
+            for i in envDist:
+                envSet += "{}=${} ".format(i, i)
+            os.system("pkexec env " + envSet + " gx-env-run " + programPath.text())
+            return
+        else:
+            os.system("gx-env-run-root " + programPath.text())
+            return
+    os.system("gx-env-run " + programPath.text())
+    sys.exit(0)
 
 app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QMainWindow()
@@ -18,6 +38,7 @@ layout.addWidget(programPath, 1, 1, 1, 3)
 controlLayout = QtWidgets.QHBoxLayout()
 cancelButton = QtWidgets.QPushButton("取消")
 okButton = QtWidgets.QPushButton("确定")
+okButton.clicked.connect(RunProgram)
 controlLayout.addWidget(runProgramWithRoot)
 controlLayout.addWidget(browser)
 controlLayout.addWidget(cancelButton)
